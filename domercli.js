913a268075155a9400a113a451e85248eb19016d
James Roland Cabresos <ashi@AsHi-PC.(none)>
2014-06-18 21:21:01 +0800

    updated usage description

--- domercli.js
+++ domercli.js
@@ -4,7 +4,7 @@ var domer = require('./src/domer');
 var program = require('commander');
 var path = require('path');
 program
-    .version('0.0.14');
+    .version('0.1.0');
 
 program
     .usage("domer -s [source] -e [encoding] -m [mode]")
